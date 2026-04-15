76051b993da96431bd6ffcb335147e009e4c8d70
Mathieu Delhoménie <mathieu.delhomenie@gmail.com>
2026-04-15 15:38:22 +0200

    feat(base-models): create_users migration

---
+++ db/migrate/20260415000001_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string  :email,           null: false
+      t.string  :password_digest, null: false
+      t.string  :first_name,      null: false
+      t.string  :last_name,       null: false
+      t.string  :role,            null: false, default: 'tenant'
+      t.string  :phone
+      t.jsonb   :data,            default: '{}'
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :role
+  end
+end
