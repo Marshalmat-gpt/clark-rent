229b07aa5f4ef4dfc483339e044849cebbda776c
Mathieu Delhoménie <mathieu.delhomenie@gmail.com>
2026-04-17 02:06:36 +0200

    chore: add Puma config

---
+++ config/puma.rb
@@ -0,0 +1,17 @@
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
+threads min_threads_count, max_threads_count
+
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+
+port ENV.fetch('PORT', 3000)
+environment ENV.fetch('RAILS_ENV', 'development')
+
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+
+if ENV.fetch('RAILS_ENV', 'development') == 'production'
+  workers ENV.fetch('WEB_CONCURRENCY', 2)
+end
+
+preload_app!
+plugin :tmp_restart
