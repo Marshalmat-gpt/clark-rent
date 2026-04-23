8f7468103b3157e99fd3163d12af7acc2162f415
Mathieu Delhoménie <mathieu.delhomenie@gmail.com>
2026-04-23 21:42:47 +0200

    debug: re-enable boot diagnostic to capture current error via GH Actions annotations

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,10 @@
 require_relative 'application'
 
-Rails.application.initialize!
+begin
+  Rails.application.initialize!
+rescue => e
+  lines = ["#{e.class}: #{e.message.split("\n").first(5).join(' | ')}",
+           e.backtrace&.first(3)&.join(' | ')].compact.join(' -- BACKTRACE: ')
+  $stderr.puts "::error::BOOT FAILURE: #{lines}"
+  raise
+end
