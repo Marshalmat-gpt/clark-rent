2307f262004d4200778a7198eb65ccb7b55ece29
Mathieu Delhoménie <203858792+Marshalmat-gpt@users.noreply.github.com>
2026-05-07 13:03:39 +0200

    fix: update ticket factory for new property/tenant/category schema

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :ticket do
-    association :reporter, factory: %i[user tenant]
-    association :room
-    title       { 'Fuite robinet cuisine' }
+    association :property
+    association :tenant, factory: %i[user tenant]
+    category    { 'plomberie' }
     description { 'Le robinet de la cuisine fuit depuis ce matin.' }
     status      { 'open' }
     priority    { 'normal' }
